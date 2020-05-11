bd9269d7c297e51db42ffc7fe9856aac3e9309d5
tedbarnett <mail@tedbarnett.com>
2020-05-10 18:13:27 -0700

    Added AkilliMum Mirror asset

---
+++ Brush/Assets/AkilliMum/Standard/Mirror/EffectBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AkilliMum.Standard.Mirror
+{
+    public class EffectBase : MonoBehaviour
+    {
+        public Dictionary<string, RenderTexture> AlreadyRendered = new Dictionary<string, RenderTexture>();
+
+        private bool _insiderendering = false;
+        public bool InsideRendering
+        {
+            get
+            {
+                return _insiderendering;
+            }
+            set
+            {
+                _insiderendering = value;
+            }
+        }
+    }
+}
